0921315e9ca0073b82158ceb159c4dbc29994b34
Aruneko <aruneko99@gmail.com>
2023-03-06 22:59:33 +0900

    display route search button

--- src/features/tombNumberSelector/components/numberSelectorWidget.tsx
+++ src/features/tombNumberSelector/components/numberSelectorWidget.tsx
@@ -7,18 +7,18 @@ export const NumberSelectorWidget: FC = () => {
 
   return (
     <div className="flex-row space-y-4 bg-white rounded-lg h-full p-4 drop-shadow-md">
-      <div className="flex-row space-y-2">
+      <div className="flex-row space-y-2 w-full">
         <div>墓地の名前を選択してください</div>
-        <select className="w-48 h-12 p-2 bg-white rounded-md border-2">
+        <select className="w-48 h-12 p-2 bg-white rounded-md border-2 w-full">
           <option value="zoshigaya">雑司ヶ谷霊園</option>
         </select>
       </div>
-      <div className="flex-row space-y-2">
+      <div className="flex-row space-y-2 w-full">
         <div>墓地番号を入力してください</div>
-        <div className="flex-row space-y-2">
-          <div className="flex items-center space-x-2">
+        <div className="flex-row space-y-2 w-full">
+          <div className="flex items-center space-x-2 w-full">
             <select
-              className="w-48 h-12 p-2 bg-white rounded-md border-2"
+              className="w-48 h-12 p-2 bg-white rounded-md border-2 w-full"
               value={tombNumber.shu} 
               onChange={onChangeShu}
             >
@@ -26,9 +26,9 @@ export const NumberSelectorWidget: FC = () => {
             </select>
             <div>種</div>
           </div>
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center space-x-2 w-full">
             <select
-              className="w-48 h-12 p-2 bg-white rounded-md border-2"
+              className="w-48 h-12 p-2 bg-white rounded-md border-2 w-full"
               value={tombNumber.go}
               onChange={onChangeGo}
             >
@@ -36,9 +36,9 @@ export const NumberSelectorWidget: FC = () => {
             </select>
             <div>号</div>
           </div>
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center space-x-2 w-full">
             <input 
-              className="w-48 h-12 p-2 bg-white rounded-md border-2"
+              className="w-48 h-12 p-2 bg-white rounded-md border-2 w-full"
               type="number"
               value={tombNumber.gawa}
               onChange={onChangeGawa}
@@ -47,7 +47,13 @@ export const NumberSelectorWidget: FC = () => {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="flex-row space-y-2 w-full">
+        <button 
+          className="w-full p-2 rounded bg-blue-500 active:bg-blue-700 text-white"
+        >
+          ルート検索
+        </button>
+      </div>
     </div>
   )
 }
