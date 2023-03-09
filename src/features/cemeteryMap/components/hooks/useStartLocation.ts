2543d7961c55a4ab5b2b28071dfbb4c4b7d35236
Aruneko <aruneko99@gmail.com>
2023-03-09 19:30:17 +0900

    use LngLat object

--- src/features/cemeteryMap/components/hooks/useStartLocation.ts
+++ src/features/cemeteryMap/components/hooks/useStartLocation.ts
@@ -1,13 +1,14 @@
 import { useCallback, useState } from "react"
 import { MarkerDragEvent } from "react-map-gl"
+import maplibreGl from "maplibre-gl"
 
 export function useStartLocation() {
-  const defaultLocation = { latitude: 35.72126, longitude: 139.71842 }
+  const defaultLocation = new maplibreGl.LngLat(139.71842, 35.72126)
   const [startLocation, setStartLocation] = useState(defaultLocation)
 
   const setLocation = useCallback((event: MarkerDragEvent) => {
     const {lng, lat} = event.lngLat
-    setStartLocation({ latitude: lat, longitude: lng })
+    setStartLocation(new maplibreGl.LngLat(lng, lat))
   }, [setStartLocation])
 
   return { startLocation, setLocation }
